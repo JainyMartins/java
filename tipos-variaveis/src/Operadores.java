acf1d85a200bb2cf17e8867960e7f1efb1f5bdd7
JainyMartins <jainyestefany@gmail.com>
2022-09-20 15:17:25 -0300

    Feature: SmartTv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    public static void main (String[] args){
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        concatenacao = 1+"1"+1+1;
+
+        concatenacao = 1+"1"+1+"1";
+
+        concatenacao = "1"+1+1+1;
+
+        concatenacao = "1"+(1+1+1);
+
+        int numero = 5;
+        System.out.println(- numero);
+    }
+}
---
+++ tipos-variaveis/src/Variaveis.java
@@ -0,0 +1,28 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        int numero = 5;
+
+        /*
+         * na linha abaixo iremos alterar o valor do varíavel para 10 observe que o tipo
+         * não é mais necessário, pois a variável já foi declarada anteriormente
+         */
+        numero = 10;
+
+        System.out.print(numero);
+
+        /*
+         * ao usar a palavra reservada final, você determina que jamais
+         * esta variavel poderá obter outro valor;
+         * logo a linha 25 vai apresentar um erro de compilação
+         * isso é considerado uma CONSTANTE na linguagem Java
+         */
+        final double VALOR_DE_PI = 3.14;
+    }
+}
