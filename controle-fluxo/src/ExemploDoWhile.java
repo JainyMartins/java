d950f34bf4a75bc303f7649a13abe67649e730ec
JainyMartins <jainyestefany@gmail.com>
2022-09-22 15:32:45 -0300

    Estruturas Condicionais e Repetição

---
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,13 @@
+// ExemploDoWhile.java
+
+import java.util.Random;
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        for(int x=1;x<2;x++){
+            numero = numero + x;
+        }
+        System.out.println("O valor de número é: " + numero);
+    }
+}
